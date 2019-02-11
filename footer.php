810e06fc7345c8bf92513b5c25f831e589b85188
Thiago <souza.ty@gmail.com>
2019-02-11 11:01:35 -0200

    remove scripts

--- footer.php
+++ footer.php
@@ -106,62 +106,6 @@
  		        $('#menu-item-257').find('a').attr('data-toggle', 'collapse');
  		    });
  		</script>
-
- 		<script>
- 				function alterar_div() {
- 					jQuery(document).ready(function($) {
-
- 						var data = {
- 							'action': 'buscaTransparencia',
- 							'ano_busca': $('#ano_busca').val()
- 						};
-
- 						// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
- 						jQuery.post('<?php echo get_site_url(); ?>/wp-admin/admin-ajax.php', data, function(response) {
- 							//alert('Got this from the server: ' + response);
- 							$('#content-transparencia').html(response);
- 						});
- 					});
-
- 				}
- 		</script>
-
- 		<script>
- 		function alteraAction(valor) {
-
- 			if (valor == "ibgh") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/transparencia-resultado/');
-     		}else if (valor == "heelj") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/hospital-estadual-ernestina-lopes-jaime/transparencia-heelj-resultado/');
-     		}else if (valor == "hma") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/hospital-municipal-de-araguaina/transparencia-hma-resultado/');
-     		}else if (valor == "upa-goianesia") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/upa-goianesia/transparencia-upa-goianesia-resultado/');
-     		}else if (valor == "upa-araguaina") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/upa-araguaina/transparencia-upa-araguaina-resultado/');
-     		}
-
- 		}
- 		</script>
-
- 		<script>
- 		function alteraActionImpresa(valor) {
-
- 			if (valor == "ibgh") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/sala-de-imprensa/');
-     		}else if (valor == "heelj") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/hospital-estadual-ernestina-lopes-jaime/sala-de-imprensa/');
-     		}else if (valor == "hma") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/hospital-municipal-de-araguaina/sala-de-imprensa/');
-     		}else if (valor == "upa-goianesia") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/upa-araguaina/sala-de-imprensa/');
-     		}else if (valor == "upa-araguaina") {
-        			 $('#busca-transparencia').attr('action', 'http://ibgh.org.br/unidades/upa-goianesia/sala-de-imprensa/');
-     		}
-
- 		}
- 		</script>
-
        	<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58e2f40f3b47d4fd"></script>
 
        <?php wp_footer(); ?>
